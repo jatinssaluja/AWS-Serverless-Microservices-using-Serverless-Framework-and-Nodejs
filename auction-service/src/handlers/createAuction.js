553277c6200c8a2c12d33d60ea18c211704ae6ed
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-06-27 02:33:03 +0200

    Use dynamodb DocumentClient to insert an auction into AuctionsTable

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,14 +1,25 @@
+import {v4 as uuid} from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
 
   const {title} = JSON.parse(event.body);
   const now = new Date();
  
   const auction = {
+    id:uuid(),
     title,
     status:'open',
     createdAt: now.toISOString()
 
   }
+
+  await dynamodb.put({
+    TableName:'AuctionsTable',
+    Item:auction
+  }).promise();
 
   return {
     statusCode: 201,
