2bc672f20f264d407f6a66c3fce17a436dfb62de
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-06-27 13:57:02 +0200

    Add middlewares and error-handling to createAuctions lambda

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,11 +1,16 @@
 import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 const dynamodb = AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
 
-  const {title} = JSON.parse(event.body);
+  const {title} = event.body;
   const now = new Date();
  
   const auction = {
@@ -16,10 +21,19 @@ async function createAuction(event, context) {
 
   }
 
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item:auction
-  }).promise();
+  try{
+
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item:auction
+    }).promise();
+
+  } catch(error){
+
+    console.error(error);
+    throw new createError.InternalServerError(error);
+
+  }
 
   return {
     statusCode: 201,
@@ -27,6 +41,12 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser()) // parse stringified event body.
+  // adjust API Gateway event objects to prevent us from
+  // having non-existent objects when trying to access non-existent 
+  // path parameters and query parameters.
+  .use(httpEventNormalizer()) 
+  .use(httpErrorHandler()); // simplifies error-handling process.
 
 
