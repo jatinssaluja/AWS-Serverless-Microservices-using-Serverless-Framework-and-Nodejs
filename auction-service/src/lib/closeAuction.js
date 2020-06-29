5a2a1c7d1ee0c381d41cc54d26023cec20b3a8a3
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-06-29 15:14:45 +0200

    Modify closeAuction lambda to handle no bidding scenario

--- auction-service/src/lib/closeAuction.js
+++ auction-service/src/lib/closeAuction.js
@@ -4,6 +4,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
+
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id: auction.id },
@@ -19,6 +20,19 @@ export async function closeAuction(auction) {
   await dynamodb.update(params).promise();
   const {title, seller, highestBid} = auction;
   const {amount, bidder} = highestBid;
+
+  if (amount === 0) {
+    await sqs.sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'No bids on your auction item',
+        recipient: seller,
+        body: `Your item ${title} didn't get any bids. Better luck next time!`,
+      }),
+    }).promise();
+    return;
+  }
+
 
   const notifySeller = sqs.sendMessage({
     QueueUrl: process.env.MAIL_QUEUE_URL,
